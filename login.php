74abc24a0cbc638ca72d7f62516d24907b97ac4f
siemdicou <siemdicou@hotmail.com>
2015-09-05 21:33:12 +0200

    hi

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	<table>
+		<tr>
+			<td>Email:</td>
+			<td><input type="text" required></td>
+		</tr>
+		<tr>
+			<td>Wachtwoord:</td>
+			<td><input type="password" name="" required></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="send"></td>
+		</tr>
+	</table>
+</form>
+
+<?php 
+	
+
+
+?>
+</body>
+</html>
