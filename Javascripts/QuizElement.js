6b20082dafcc0a1c3257898551ee3dfb73141dd9
Chanisco <chanisco@live.nl>
2015-09-05 17:41:31 +0100

    Quiz element Added

---
+++ Javascripts/QuizElement.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var QuestionField;
+
+function _get(d){
+     document.getElementById(d);
+
+}
+
+function Start() {
+    QuestionField = _get("QuestionField");
+}
+
+window.onload(Start);
