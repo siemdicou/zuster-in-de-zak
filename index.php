3a751465d57313450736bb5195586417e1821ebf
Banath22 <casper_vz@hotmail.com>
2015-09-05 17:35:09 +0200

    Hi

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 <html>
 <head>
 	<title>Zuster in de zak</title>
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+<div class="wrapper">
+	<div class="app">
+		<h1>Titel</h1>
+		<?php
+			for ($i=0; $i < 5; $i++) { 
+				$menu = ["Quizz","Mijn gegevens","Agenda","Pillen","Dagboek"];
+				if ($i%2) {
+					
+					echo "<p class='white'>".$menu[$i]."</p>";
+					
+				}
+				else{
+					echo "<p class='blue'>".$menu[$i]."</p>";
+				}
+			}
+		?>
+	</div>
+</div>
 
 </body>
 </html>
