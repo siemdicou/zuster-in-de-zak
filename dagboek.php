b2d28703c45bf2e3882d9751ea7ddbb5f59f060c
siemdicou <siemdicou@hotmail.com>
2015-09-06 13:44:34 +0200

    the game

--- dagboek.php
+++ dagboek.php
@@ -7,7 +7,7 @@
 <body>
 <div class="wrapper">
 <textarea rows="15" cols="50"autofocus required> vul hier u dagboek in  </textarea>
-<input type="submit">
+<input type="submit" id="dagboekSend" width="50px">
 </div>
 </body>
 </html>
